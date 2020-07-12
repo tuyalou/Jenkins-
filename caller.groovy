4ed062558a327b1e4e6ccc9f7d516be8950c41ea
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-12 16:22:10 -0500

    Create caller.groovy

---
+++ caller.groovy
@@ -0,0 +1,23 @@
+node {
+	stage("Stage1"){
+		echo "hello"
+}
+	stage("Stage2"){
+		echo "hello"
+        // Calls another job called "Timestamp"
+        build 'Timestamp'
+}
+	stage("Stage3"){
+		echo "hello"
+        // Calls another job called "Script"
+        build "Script"
+}
+	stage("Stage4"){
+		build 'Template2'
+        // Calls another job called "Script"
+}
+	stage("Stage5"){
+		echo "hello"
+	}
+    
+}
