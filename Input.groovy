b6c3f9e428fda6e088ae013a9bb08878acb5e7c8
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-12 16:17:50 -0500

    Create Input.groovy

---
+++ Input.groovy
@@ -0,0 +1,18 @@
+def deployer(ENVIR){
+	node {
+		stage("Stage1"){
+			echo "hello"
+		}
+		stage("Stage2"){
+			echo "hello"
+		}
+		stage("Stage3"){
+			echo "hello"
+		}
+		stage("Ask for Input"){
+			input 'Should I proceed?'
+		}
+			stage("Stage5"){
+		}
+	}
+}
